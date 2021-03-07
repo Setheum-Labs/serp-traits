79777576d60618b1e0d388fcdc12968df0098ab8
Muhammad-Jibril <jbashir52@gmail.com>
2021-03-08 05:58:57 +0800

    Update serp_market.rs

--- src/serp_market.rs
+++ src/serp_market.rs
@@ -4,7 +4,7 @@ use codec::{Decode, Encode};
 use frame_support::Parameter;
 use sp_runtime::{
 	traits::{AtLeast32Bit, Bounded, CheckedDiv, MaybeSerializeDeserialize, Member},
-	DispatchError, DispatchResult, RuntimeDebug,
+	DispatchError, DispatchResult, RuntimeDebug, PerThing, Perbill,
 };
 use sp_std::{
 	cmp::{Eq, PartialEq},
@@ -16,106 +16,112 @@ use sp_std::{
 /// Serping info.
 #[cfg_attr(feature = "std", derive(PartialEq, Eq))]
 #[derive(Encode, Decode, RuntimeDebug)]
-pub struct SerpingInfo<AccountId, Balance, BlockNumber> {
-	/// Current trader, trade price and the blocknumber of trade.
-	pub trade: Option<(AccountId, Balance, BlockNumber)>,
-}
+/// A `SerpMarketPriceProvider` implementation based on price data from a `DataProvider`
+pub struct DefaultSerpMarketPriceProvider<CurrencyId, Source>(PhantomData<(CurrencyId, Source)>);
 
-/// Abstraction over a simple serping system.
-pub trait SerpMarket<AccountId,  Balance, BlockNumber, SerpingId> {
-	/// The id of a SerpingInfo
-	type SerpingId: FullCodec + Default + Copy + Eq + PartialEq + MaybeSerializeDeserialize + Bounded + Debug;
+/// Abstraction over a serping market system for the Setheum Elastic Reserve Protocol (SERP) Market.
+pub trait SerpMarket<CurrencyId, AccountId,  Balance, Price, BlockNumber> {
 	/// The price to trade.
 	type Balance: AtLeast32Bit + FullCodec + Copy + MaybeSerializeDeserialize + Debug + Default;
     /// The currency type in trade.
 	type CurrencyId: FullCodec + Eq + PartialEq + Copy + MaybeSerializeDeserialize + Debug;
-    /// The currency price
+    /// The currency price type.
 	type Price = FixedU128;
 
-	/// The serping info of `id`
-	fn serping_info(id: Self::SerpingId) -> Option<SerpingInfo<AccountId, Self::Balance, BlockNumber>>;
+    // Public immutables
+    
+    /// Provide relative `price` for two currencies
+    fn get_price(
+        base: CurrencyId, 
+        quote: CurrencyId,
+    ) -> Option<Price>;
 
-    /// The serping info of `id`
-	fn mint_rate(id: Self::SerpingId) -> Option<SerpingInfo<AccountId, Self::Balance, BlockNumber>>;
+    /// Provide relative `serping_price` for two currencies
+    /// with additional `serp_quote`.
+	fn get_serping_price(
+        base: CurrencyId, 
+        quote: CurrencyId,
+        mint_rate: Perbill,
+    ) -> Option<Price>;
 
-    /// Add `amount` to the balance of `who` under `currency_id` and increase
-	/// total issuance.
-	fn on_(currency_id: Self::CurrencyId, who: &AccountId, amount: Self::Balance) -> DispatchResult;
+    // Public mutables
 
-	/// Remove `amount` from the balance of `who` under `currency_id` and reduce
-	/// total issuance.
-	fn withdraw(currency_id: Self::CurrencyId, who: &AccountId, amount: Self::Balance) -> DispatchResult;
-
-    /// Called when supply expansion is received from the SERP.
-	/// The return value determines if the trade should be accepted and update
-	/// serping end time. Implementation should reserve money from current
-	/// winner and refund previous winner.
+	/// Called when `expand_supply` is received from the SERP.
+	/// Implementation should `deposit` the `amount` to `serpup_to`, 
+	/// then `amount` will be slashed from `serpup_from` and update
+	/// `new_supply`.
 	fn on_expand_supply(
-		now: BlockNumber,
-		id: SerpingId,
-		new_trade: (AccountId, Balance),
-		last_trade: Option<(AccountId, Balance)>,
-	) -> OnNewTradeResult<BlockNumber>;
+        currency_id: CurrencyId,
+		amount: Balance,
+		serpup_to: AccountId, AccountId,
+		serpup_from: AccountId,
+		new_supply: Balance,
+	) -> DispatchResult;
 
-	/// Called when new trade is received.
-	/// The return value determines if the trade should be accepted and update
-	/// serping end time. Implementation should reserve money from current
-	/// winner and refund previous winner.
+	/// Called when `contract_supply` is received from the SERP.
+	/// Implementation should `deposit` the `base_currency_id` (The Native Currency) 
+    /// of `amount` to `serpup_to`, then `amount` will be slashed from `serpup_from` 
+	/// and update `new_supply`.
 	fn on_contract_supply(
-		now: BlockNumber,
-		id: SerpingId,
-		new_trade: (AccountId, Balance),
-		last_trade: Option<(AccountId, Balance)>,
-	) -> OnNewTradeResult<BlockNumber>;
-
+		currency_id: CurrencyId,
+		amount: Balance,
+		serpdown_to: AccountId,
+		serpdown_from: AccountId,
+		new_supply: Balance,
+	) -> DispatchResult;
 }
 
-/// A trait to provide relative price for two currencies
-pub trait MarketPriceProvider<CurrencyId, Price> {
+/// A trait to provide relative markey price for two currencies
+pub trait SerpMarketPriceProvider<CurrencyId, Price> {
 	fn get_price(base: CurrencyId, quote: CurrencyId) -> Option<Price>;
+	fn get_serping_price(base: CurrencyId, quote: CurrencyId) -> Option<Price>;
 }
+
 
 /// A `MarketPriceProvider` implementation based on price data from a `DataProvider`
 pub struct DefaultMarketPriceProvider<CurrencyId, Source>(PhantomData<(CurrencyId, Source)>);
 
-impl<CurrencyId, Source, Price> PriceProvider<CurrencyId, Price> for DefaultPriceProvider<CurrencyId, Source>
+impl<CurrencyId, Source, Price> MarketPriceProvider<CurrencyId, Price> for DefaultMarketPriceProvider<CurrencyId, Source>
 where
 	CurrencyId: Parameter + Member + Copy + MaybeSerializeDeserialize,
 	Source: DataProvider<CurrencyId, Price>,
 	Price: CheckedDiv,
+    SerpingQuote: CheckedAdd,
+    MintRate: Perbill
 {
-	fn get_price(base_currency_id: CurrencyId, quote_currency_id: CurrencyId) -> Option<Price> {
-		let base_price = Source::get(&base_currency_id)?;
+	fn get_serpup_price(base_currency_id: CurrencyId, quote_currency_id: CurrencyId) -> Option<Price> {
+		let base_price = Source::get(&base_currency_id)?; // base currency price compared to currency (native currency could work best)
 		let quote_price = Source::get(&quote_currency_id)?;
-
-		base_price.checked_div(&quote_price)
+        let market_price = base_price.checked_div(&quote_price); // market_price of the currency.
+        let mint_rate = Perbill::from_percent(); // supply change of the currency.
+        let serp_quote = market_price.checked_add(Perbill::from_percent(&mint_rate * 2)); // serping_price of the currency.
+        serp_quote.checked_add(Perbill::from_percent(&mint_rate * 2)); 
 	}
 }
 
 /// Hooks for serping to handle trades.
-pub trait MarketHandler<AccountId, Balance, BlockNumber, SerpingId> {
-	/// Called when supply expansion is received from the SERP.
-	/// The return value determines where to `deposit` the `new_supply` to `serpup_to` and update
-	/// supply. Implementation should deposit `amount` to `serpup_to`, and in the case of Setheum,
-	/// `amount` will be slashed from `serpup_from`.
+pub trait SerpMarketHandler<AccountId, Balance, BlockNumber, SerpingId> {
+	/// Called when `expand_supply` is received from the SERP.
+	/// Implementation should `deposit` the `amount` to `serpup_to`, 
+	/// then `amount` will be slashed from `serpup_from` and update
+	/// `new_supply`.
 	fn on_expand_supply(
         currency_id: CurrencyId,
 		amount: Balance,
-		serpup_to: AccountId,
+		serpup_to: AccountId, AccountId,
 		serpup_from: AccountId,
 		new_supply: Balance,
-		id: SerpingId,
 	) -> DispatchResult;
 
-
-	/// Called when new trade is received.
-	/// The return value determines if the trade should be accepted and update
-	/// serping end time. Implementation should reserve money from current
-	/// winner and refund previous winner.
+	/// Called when `contract_supply` is received from the SERP.
+	/// Implementation should `deposit` the `base_currency_id` (The Native Currency) 
+    /// of `amount` to `serpup_to`, then `amount` will be slashed from `serpup_from` 
+	/// and update `new_supply`.
 	fn on_contract_supply(
-		now: BlockNumber,
-		id: SerpingId,
-		new_trade: (AccountId, Balance),
-		last_trade: Option<(AccountId, Balance)>,
-	) -> OnNewTradeResult<BlockNumber>;
+		currency_id: CurrencyId,
+		amount: Balance,
+		serpdown_to: AccountId,
+		serpdown_from: AccountId,
+		new_supply: Balance,
+	) -> DispatchResult;
 }
