5e3aec0cd8f63556f50f10eae91ec5d2a7ae24db
Muhammad-Jibril <jbashir52@gmail.com>
2021-03-11 01:53:49 +0800

    rearrange

--- src/lib.rs
+++ src/lib.rs
@@ -21,13 +21,13 @@ pub use serp_market::Market;
 pub use serp_tes::{SerpTes, ElastAdjustmentFrequency};
 pub use setheum_currency::{Currency, LockableCurrency, ReservableCurrency};
 pub use stp258_currency::{
-	Stp258Asset, Stp258AssetExtended, Stp258AssetLockable, 
+	OnDust, Stp258Asset, Stp258AssetExtended, Stp258AssetLockable, 
 	Stp258AssetReservable, Stp258Currency, Stp258CurrencyExtended, 
-	Stp258CurrencyLockable, Stp258CurrencyReservable, OnDust,
+	Stp258CurrencyLockable, Stp258CurrencyReservable,
 };
 pub use stp258_stable_currency::{
-	Stp258StableCurrency, Stp258StableCurrencyExtended, 
-	Stp258StableCurrencyLockable, Stp258StableCurrencyReservable, OnStp258Dust,
+	OnStp258Dust, Stp258StableCurrency, Stp258StableCurrencyExtended, 
+	Stp258StableCurrencyLockable, Stp258StableCurrencyReservable,
 };
 
 pub mod account;
