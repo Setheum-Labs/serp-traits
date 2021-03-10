6987d29b69e625fc18b734309d8bdc96160e0f71
Muhammad-Jibril <jbashir52@gmail.com>
2021-03-11 07:47:41 +0800

    remove burn_reserved and create_reserved

--- src/stp258_currency.rs
+++ src/stp258_currency.rs
@@ -139,18 +139,6 @@ pub trait Stp258CurrencyReservable<AccountId>: Stp258Currency<AccountId> {
 	/// be returned.
 	fn slash_reserved(currency_id: Self::CurrencyId, who: &AccountId, value: Self::Balance) -> Self::Balance;
 
-	/// Burns `value` from reserved balance of `who`. This function 
-	/// returns `TotalIssuanceUnderflow` as error if underflow. Increases `total_issuance`.
-	/// It changes `TotalIssuance`.
-	/// Is a no-op if the `value` to be deposited is zero.
-	fn burn_reserved(currency_id: Self::CurrencyId, who: &AccountId, value: Self::Balance,) -> DispatchResult;
-
-	/// Mints `value` to reserved balance of `who`. This function 
-	/// returns `TotalIssuanceOverflow` as error if overflow. Increases `total_issuance`.
-	/// It changes `TotalIssuance`.
-	/// Is a no-op if the `value` to be deposited is zero.
-	fn create_reserved(currency_id: Self::CurrencyId, who: &AccountId, value: Self::Balance) -> DispatchResult;
-
 	/// The amount of the balance of a given account that is externally
 	/// reserved; this can still get slashed, but gets slashed last of all.
 	///
