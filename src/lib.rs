b7b16b65628955b755e0dd3e42951aff3c48d351
Muhammad-Jibril <jbashir52@gmail.com>
2021-03-11 08:51:49 +0800

    add balancestatus and lockidentifier use

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ pub use serp_market::Market;
 pub use serp_tes::{SerpTes, ElastAdjustmentFrequency};
 pub use setheum_currency::{Currency, LockableCurrency, ReservableCurrency};
 pub use stp258_currency::{
+	BalanceStatus, LockIdentifier,
 	OnDust, Stp258Asset, Stp258AssetExtended, Stp258AssetLockable, 
 	Stp258AssetReservable, Stp258Currency, Stp258CurrencyExtended, 
 	Stp258CurrencyLockable, Stp258CurrencyReservable,
